f0712a1e5e3c6b407c128f371f1f560e5872ad2e
sree1786 <62541317+sree1786@users.noreply.github.com>
2021-06-23 16:53:08 +0530

    Update scripstart.sh
    updated

--- scripstart.sh
+++ scripstart.sh
@@ -1,15 +1,21 @@
-#!/opt/rh/rh-python36/root/usr/bin/python3.6
+#!/bin/python3
 import requests,json,smtplib,sys,threading,uuid,random,uuid,re,ssl,socket,textwrap,argparse
 from datetime import *
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import time as t
 from os import system
+
+ 
+
 aid = sys.argv[1]
-passwd = sys.argv[2]
-vmname = sys.argv[3]
+token = sys.argv[2]
+box = sys.argv[3]
+
+ 
+
 def auth_token():
         url = "https://api.cloud.fmr.com/auth"
-        headers = {'X-Auth-User':"aid",'X-Auth-Key':"passwd"}
+        headers = {'X-Auth-User':"aid",'X-Auth-Key':"token"}
         response = requests.request("GET",url,headers=headers,verify='FECA.cer')
         if response.status_code == 200:
                 print('Auth Token obtained Successfully ')
@@ -26,39 +32,24 @@ def auth_token():
 
 
 def get_vmid(vmname):
-        url = "https://api.cloud.fmr.com/vms/vm_summary?hostname="+vmname
+        url = "http://api.cloud.fmr.com/vms/vm_summary?hostname="+vmname
         headers = {'X-Auth-Token': auth_token()}
         response= requests.get(url, headers=headers,verify='FECA.cer')
         data = json.loads(response.text)
-        if 'msg' in data:
-                print(vmname,'--> Not able to get VM Details')
-                sys.stdout.flush()
-        else:
-                #vmstop(data['vm_id'],vmname)
-                vmstart(data['vm_id'],vmname)
+        print(data)
+        #        print(vmname,'--> Not able to get VM Details')
+        #        sys.stdout.flush()
+        #        failed_list.append(vmname)
+        #else:
+        #        details=data['vm_id']
+        #        print(details)
 
  
 
-def vmdata(idnumber,name):
+
+def patchvm(idnumber,name):
         headers = {'X-Auth-Token': auth_token()}
-        url="https://api.cloud.fmr.com/vms/"+str(idnumber)+"/index"
-        response= requests.get(url, headers=headers,verify='FECA.cer')
-        data = json.loads(response.text)
-        if response.text != "null":
-                print(data['primary_hostname']+'-->'+data['vmname']+'-->'+data['state'])
-                sys.stdout.flush()
-        else:
-                server=server+name+","
-                print(name)
-        sys.stdout.flush()
-
- 
-
- 
-
-def vmstop(idnumber,name):
-        headers = {'X-Auth-Token': auth_token()}
-        url="https://api.cloud.fmr.com/vms/"+str(idnumber)+"/stop"
+        url="https://api.cloud.fmr.com/vms/"+str(idnumber)+"/patch"
         response= requests.post(url, headers=headers,verify='FECA.cer')
         data = json.loads(response.text)
         if response.text != "null":
@@ -87,8 +78,4 @@ def vmstart(idnumber,name):
 
  
 
-auth_token() 
-print( vmname,aid,passwd)
-#get_vmid(vmname)
-
-
+get_vmid(box)
